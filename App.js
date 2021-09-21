c1a6755d24bf3d07efd78dfde117afe4aae3736e
nodev918 <devnode77@gmail.com>
2021-09-21 20:19:08 +0800

    hh

--- App.js
+++ App.js
@@ -1,11 +1,74 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import logo from './assets/img/1.jpg'
+import * as ImagePicker from 'expo-image-picker'
+import * as Sharing from 'expo-sharing'
+import uploadToAnonymousFilesAsync from 'anonymous-files'; 
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = React.useState(null)
+
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync()
+
+    if (permissionResult.granted === false) {
+      alert("Permission to accedss camera roll is required!")
+      return
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync()
+
+    if (pickerResult.cancelled === true) {
+      return
+    }
+
+    setSelectedImage({ localUri: pickerResult.uri })
+    console.log("log: ", pickerResult)
+
+    if (Platform.OS === 'web') {
+      let remoteUri = await uploadToAnonymousFilesAsync(pickerResult.uri);
+      setSelectedImage({ localUri: pickerResult.uri, remoteUri });
+    } else {
+      setSelectedImage({ localUri: pickerResult.uri, remoteUri: null });
+    } 
+  }
+
+  let openShareDialogAsync = async () => {
+    if (!(await Sharing.isAvailableAsync())) {
+      alert(`Uh oh, sharing isn't available on your platform`);
+      return;
+    }
+
+    await Sharing.shareAsync(selectedImage.localUri);
+  };
+
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: selectedImage.localUri }}
+          style={styles.thumbnail}
+        />
+        <TouchableOpacity onPress={openShareDialogAsync} style={styles.button}>
+          <Text style={styles.buttonText}>Share this photo</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} style={styles.logo} />
+      <Text style={{ color: 'blue' }}>hello world</Text>
+      <TouchableOpacity
+        onPress={openImagePickerAsync}
+        style={{ backgroundColor: 'blue' }}
+      >
+        <Text style={{ fontSize: 20, color: 'white' }}>
+          Pick a photo
+        </Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +77,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: { width: 305, height: 159 },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain"
+  }
 });
